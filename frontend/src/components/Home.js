c93b373b0812ec72f1a87a1d9fa2c157e35f0947
Aarnav Singh <aarnavsingh@Aarnavs-MacBook-Air.local>
2024-06-17 15:12:50 +0530

    added content to homepage and updated respective component styling in auth.css

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,16 +1,39 @@
 // src/components/Home.js
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Navbar from './Navbar';
 import '../auth.css';
 
 const Home = () => {
   return (
-    <div className="page-container">
+    <div>
       <Navbar />
-      <div className="auth-container">
-        <div className="auth-form">
-          <h2>Home</h2>
-          <p>Welcome to the Home Page!</p>
+      <div className="home-container">
+        <div className="home-boxes">
+          <Link to="/it" className="home-box">
+            <div className="home-box-content">
+              <img src={require('../icons/it.png')} alt="IT" />
+              <h3>IT</h3>
+            </div>
+          </Link>
+          <Link to="/sap" className="home-box">
+            <div className="home-box-content">
+              <img src={require('../icons/sap.png')} alt="SAP" />
+              <h3>SAP</h3>
+            </div>
+          </Link>
+          <Link to="/other" className="home-box">
+            <div className="home-box-content">
+              <img src={require('../icons/other.png')} alt="Other" />
+              <h3>Other</h3>
+            </div>
+          </Link>
+          <Link to="/contact-support" className="home-box">
+            <div className="home-box-content">
+              <img src={require('../icons/contact.png')} alt="Contact Support" />
+              <h3>Contact Support</h3>
+            </div>
+          </Link>
         </div>
       </div>
     </div>
