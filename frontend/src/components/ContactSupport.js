2c477ce1a08a14f13f9182aa4ddd31a00bd7ac26
Aarnav Singh <aarnavsingh@Aarnavs-MacBook-Air.local>
2024-06-17 16:36:21 +0530

    updates to styling and content of ContactSupport page

--- frontend/src/components/ContactSupport.js
+++ frontend/src/components/ContactSupport.js
@@ -6,27 +6,32 @@ import '../auth.css';
 const ContactSupport = () => {
   const contactDetails = [
     {
-      company: 'Company A',
+      company: 'Premier Energies Pvt Ltd',
       email: 'support@companya.com',
+      code: '1000',
       phone: '123-456-7890',
     },
     {
       company: 'Company B',
+      code: '2000',
       email: 'support@companyb.com',
       phone: '123-456-7891',
     },
     {
       company: 'Company C',
+      code: '3000',
       email: 'support@companyc.com',
       phone: '123-456-7892',
     },
     {
       company: 'Company D',
+      code: '4000',
       email: 'support@companyd.com',
       phone: '123-456-7893',
     },
     {
       company: 'Company E',
+      code: '5000',
       email: 'support@companye.com',
       phone: '123-456-7894',
     },
@@ -42,6 +47,7 @@ const ContactSupport = () => {
             {contactDetails.map((contact, index) => (
               <li key={index} className="contact-item">
                 <h3>{contact.company}</h3>
+                <h3>{contact.code}</h3>
                 <p>Email: <a href={`mailto:${contact.email}`}>{contact.email}</a></p>
                 <p>Phone: <a href={`tel:${contact.phone}`}>{contact.phone}</a></p>
               </li>
